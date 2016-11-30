9e3b24de75bf76d75eb7cc312d706212b22ff6dd
entr6comp <entr6comp@gmail.com>
2016-11-30 10:28:05 -0200

    arquivos E6

---
+++ Compilador/Compilador/Expressao.cs
@@ -0,0 +1,57 @@
+﻿using Antlr4.Runtime;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Compilador
+{
+    class Expressao
+    {
+        MMMLLexer lexer;
+        MMMLParser parser;
+
+        public Expressao()
+        {
+            try
+            {
+                string caminhoApp = AppDomain.CurrentDomain.BaseDirectory;
+                caminhoApp = caminhoApp.Substring(0, caminhoApp.IndexOf("bin") - 1);
+                string arquivo = Path.Combine(caminhoApp, "entrada.txt");
+
+                StreamReader file = new StreamReader(arquivo);
+                lexer = new MMMLLexer(new AntlrInputStream(file));
+                CommonTokenStream tokens = new CommonTokenStream(lexer);
+                parser = new MMMLParser(tokens);
+
+                var res = parser.funcbody();
+
+                string msgRetorno = string.Empty;
+
+                if (parser.qtdErro > 0)
+                    msgRetorno = "Expressão inválida";
+                else
+                {
+                    Stack<object> pilhaSimbolo = parser.PilhaSimbolo;
+                    if (pilhaSimbolo != null && pilhaSimbolo.Count >= 1)
+                    {
+                        object resultado = pilhaSimbolo.Pop();
+                        msgRetorno = string.Format("Resultado final: {0} (type {1})", resultado, res.oType.Name);
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(msgRetorno))
+                {
+                    Console.WriteLine(msgRetorno);
+                    Console.WriteLine("-------------------------------");
+                }
+
+                file.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro: " + ex);
+                return;
+            }
+        }
+    }
+}
