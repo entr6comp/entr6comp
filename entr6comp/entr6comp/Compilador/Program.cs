398c91398076fc94ebb22ca1ca561e440d42a7c6
entr6comp <entr6comp@gmail.com>
2016-11-30 10:10:38 -0200

    arquivos e6

---
+++ entr6comp/entr6comp/Compilador/Program.cs
@@ -0,0 +1,16 @@
+﻿using Antlr4.Runtime;
+using System;
+using System.Globalization;
+using System.Threading;
+
+namespace Compilador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            Expressao ex = new Expressao();
+        }
+    }
+}
